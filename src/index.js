c1bb49f3b302f127480ca4fee97b8209f73240ce
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-09 01:10:34 +0900

    Apply BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import 'normalize.css';
 import 'styles.css';
 import App from 'components/App';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
-document.getElementById('root'));
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

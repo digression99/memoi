6225a3b3b51797006eed05a8b965732d1d4e5ad9
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-08 11:29:04 +0900

    Create Memos

---
+++ src/components/Memos/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Memo from 'components/Memo';
+import styled from 'styled-components';
+
+const Memos = styled.div`
+  display : flex;
+  flex-direction : row;
+  flex-flow : wrap;
+  justify-content : space-around;
+  max-width : 700px;
+  padding : 4px;
+`;
+
+export default ({ memos }) => {
+
+  return (
+    <Memos>
+      {memos.map(memo => <Memo key={memo.id} {...memo} />)}
+    </Memos>
+  )
+};

bc2fa84fee8b88c63a71cc43c2803fa6cf08621a
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-09 01:12:51 +0900

    Change reducers to handle hashtags

--- src/reducers.js
+++ src/reducers.js
@@ -4,19 +4,21 @@ export default (state, action) => {
   switch (action.type) {
     case "ADD_MEMO":
       console.log('add memo received.');
+      const { contents, hashtags  } = action.payload;
 
       const newMemo = {
         id : generateUuid(),
         title : '',
-        contents : action.payload,
-        hashtags : []
+        contents,
+        hashtags
       };
 
       const newState = {
         ...state,
         memos : [...state.memos, newMemo]
-      }
+      };
       console.log('new state : ', newState);
+
       return newState;
     default:
       return state;

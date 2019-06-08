b700cef1d27c65eaf5e2d0f4008ae4674fc1a656
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-08 12:11:55 +0900

    Create MemoForm

---
+++ src/components/MemoForm/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ContentInput = styled.input`
+  min-width : 300px;
+  max-width : 500px;
+  height : 2.6rem;
+  border : none;
+  outline : none;
+
+  border-radius : 10px;
+  margin : 20px 0 20px 0;
+  font-size : 1.2rem;
+  transition : all .3s;
+  box-shadow : 0 0 5px 3px rgba(0, 0, 0, 0.3);
+  background : #e9ecef;
+  padding : 6px 0 6px 10px;
+
+  &:focus {
+    background : white;
+  }
+
+  &:hover {
+  }
+`;
+
+const MemoFormContainer = () => {
+  const [content, setContent] = useState("");
+
+  const handleContentChange = e => {
+    setContent(e.target.value);
+  };
+
+  const handleKeyDown = e => {
+    console.log('key down.');
+    console.log('e.key : ', e.key);
+
+    if (e.key === 'Enter') {
+      setContent('');
+    }
+  };
+
+  return (
+    <div onKeyDown={handleKeyDown}>
+      <ContentInput 
+        type="text" 
+        name="content" 
+        placeholder="Enter any link"
+        onChange={handleContentChange}
+        value={content}
+      />
+    </div>
+  );
+};
+
+export default MemoFormContainer;

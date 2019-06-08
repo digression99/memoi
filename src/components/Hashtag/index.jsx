ac32ada484190ad2e33add5a495e88033b87d718
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-09 01:11:59 +0900

    Change Hashtag to render Link

--- src/components/Hashtag/index.jsx
+++ src/components/Hashtag/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Hashtag = styled.span`
+const Hashtag = styled(Link)`
   display : inline-block;
   background : #adb5bd;
   border-radius : 100px;
   padding : 4px 8px;
   margin : 4px 4px 4px 0;
-  cursor : pointer;
   color : #f1f3f5;
   font-size : 0.8rem;
+
+  &:link, &:visited {
+    text-decoration : none;
+  }
 `;
 
 export default ({ tag }) => {
 
-  const handleClickTag = (e) => {
-    e.preventDefault();
-    console.log('You clicked : ', tag);
-  };
-
   return (
-    <Hashtag onClick={handleClickTag}>{tag}</Hashtag>
+    <Hashtag to={`/hashtag/${tag}`}>{tag}</Hashtag>
   )
 };

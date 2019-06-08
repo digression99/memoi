37c7dded42d497b04cd907d24dfc1bedf2c6e2d0
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-09 01:10:58 +0900

    Create Hashtag page

---
+++ src/pages/Hashtag/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import Memos from 'components/Memos';
+import MemoForm from 'components/MemoForm';
+import Header from 'components/Header';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction : column;
+  max-width : 700px;
+  padding-top : 40px;
+`;
+
+export default ({ tag }) => {
+  const memos = useSelector((state) => state.memos.filter(memo => memo.hashtags.includes(tag)));
+
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <MemoForm />
+        <Memos memos={memos} />
+      </Wrapper>
+    </>
+  )
+};

7ce38c59771e0d3e0ff131bfd3cdbeb69a787f48
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-15 21:07:13 +0900

    Create Group page

--- src/pages/Group/index.jsx
+++ src/pages/Group/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from 'components/Header';
+import LeftSideBar from 'components/LeftSideBar';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction : row;
+  height : 100%;
+`;
 
 export default () => {
   return (
-    <div>
-      This is group page.
-    </div>
+    <>
+      <Header />
+      <Wrapper>
+        <LeftSideBar selected={'group'} />
+        <div>
+          This is group page
+        </div>
+      </Wrapper>
+    </>
   )
 };
 

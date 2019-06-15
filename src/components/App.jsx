bb64300efc676c9f2dcc2f4c915c0b427eb687e0
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-15 21:08:04 +0900

    Add Group, Link pages to router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from 'pages/Main';
-import Hashtag from 'pages/Hashtag';
+import MainPage from 'pages/Main';
+import HashtagPage from 'pages/Hashtag';
+import LinkPage from 'pages/Link';
+import GroupPage from 'pages/Group';
 
-function App() {
+const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/hashtag/:tag" component={({ history, location, match }) => <Hashtag tag={match.params.tag} />} />
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/hashtag/:tag" component={({ history, location, match }) => <HashtagPage tag={match.params.tag} />} />
+      <Route exact path="/groups" component={GroupPage} />
+      <Route exact path="/links" component={LinkPage} />
     </Switch>
   );
 }

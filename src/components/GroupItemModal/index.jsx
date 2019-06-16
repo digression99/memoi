9973fee9adce0a68400d2553e7708696543160b8
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-16 16:32:41 +0900

    Use Modal in GroupItemModal

--- src/components/GroupItemModal/index.jsx
+++ src/components/GroupItemModal/index.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-
-const Background = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.3);
-  z-index: 10001;
-
-  display : flex;
-  justify-content : center;
-  align-items : center;
-`;
+import Modal from 'components/Modal';
 
 const Container = styled.div`
   width : 50%;
@@ -36,13 +22,13 @@ const GroupItemModal = ({ onClose, isOpen, children }) => {
     e.stopPropagation();
   };
 
-  return ReactDOM.createPortal(
-    <Background onClick={handleBackgroundClick}>
+  return (
+    <Modal onClickBackground={handleBackgroundClick}>
       <Container onClick={onContainerClick}>
         {children}
       </Container>
-    </Background>
-    , document.body);
+    </Modal>
+  )
 };
 
 export default GroupItemModal;

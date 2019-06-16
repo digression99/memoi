e6a1cdf33ee347cb9af95ef4daf2f1caab042a4c
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-16 16:21:22 +0900

    Use MemoMenu in Memo

--- src/components/Memo/Memo.jsx
+++ src/components/Memo/Memo.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Hashtags from 'components/Hashtags';
+import dotMenuIcon from 'resources/icons/dot-menu.png';
 import { Link } from 'react-router-dom';
+import MemoMenu from 'components/MemoMenu';
 
 const Wrapper = styled.div`
   display : flex;
-  flex-direction : column;
-
+  flex-direction: row;
   border-radius : 10px;
   border : 1px solid rgba(0, 0, 0, 0.2);
-  padding : 0.6rem 1rem;
-  width : 300px;
+  padding : 10px 5px;
+  max-width : 300px;
   max-height : 200px;
   margin : 4px;
+`;
+
+const Left = styled.div`
+  display : flex;
+  flex-direction : column;
+  max-width : 200px;
 `;
 
 const Contents = styled.div`
   word-wrap : break-word;
   margin: 10px 0 10px 0;
   font-size : 0.8rem;
+`;
+
+const Right = styled.div`
+  margin-left : 10px;
+`;
+
+const MenuButton = styled.button`
+  position: relative;
+  outline: none;
+  border: none;
+  background: none;
+  width: 28px;
+  cursor: pointer;
+  & > img {
+    width: 100%;
+  }
 `;
 
 const Span = ({ text }) => <><span>{text}</span>{' '}</>
@@ -29,6 +52,11 @@ const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\
 const hashtagRegex = /(?:\s|^)#[A-Za-z0-9\-\.\_]+(?:\s|$)/;
 
 export default ({ contents, hashtags }) => {
+  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+  const handleMenuButtonClick = e => {
+    console.log('handleMenuButtonClick');
+    setIsMenuOpen(true);
+  };
 
   const parseContents = (contents) => {
     const splitStr = contents.split(' ');
@@ -47,10 +75,22 @@ export default ({ contents, hashtags }) => {
     return componentList;
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <Wrapper>
-      <Contents>{parseContents(contents)}</Contents>
-      <Hashtags hashtags={hashtags} />
+      <Left>
+        <Contents>{parseContents(contents)}</Contents>
+        <Hashtags hashtags={hashtags} />
+      </Left>
+      <Right>
+        <MenuButton onClick={handleMenuButtonClick}>
+          <img src={dotMenuIcon} alt="menu" />
+          {isMenuOpen && <MemoMenu onClose={closeMenu} />}
+        </MenuButton>
+      </Right>
     </Wrapper>
   )
 };

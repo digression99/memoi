d3ab72db41bd14a71234d3fc350ad6a51ef6ef4a
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-22 23:08:01 +0900

    Modify header style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import homeIcon from 'resources/icons/home.png';
 import MemoForm from 'components/MemoForm';
 import SearchForm from 'components/SearchForm';
 
-const Wrapper = styled.div`
+const Header = styled.div`
   padding : 20px 10px;
   display : flex;
   justify-content : space-between;
   width : 100%;
-`;
-
-const Left = styled.div`
-  width : 200px;
-  display : flex;
-  align-items: center;
-  & > *:not(:first-child) {
-    margin-left : 10px;
-  }
-  transition : all .3s;
-  flex : ${ ({ focused }) => focused ? 'auto' : 'initial'};
 `;
 
 const StyledSearchForm = styled(SearchForm)`
@@ -32,36 +19,19 @@ export default () => {
   const [ isMemoFormFocused, setIsMemoFormFocused ] = useState(false);
   const [ isSearchFormFocused, setIsSearchFormFocused ] = useState(false);
 
-  const handleMemoFormFocus = () => {
-    setIsMemoFormFocused(true);
-  };
-
-  const handleMemoFormBlur = () => {
-    setIsMemoFormFocused(false);
-  };
-
-  const handleSearchFormFocus = () => {
-    setIsSearchFormFocused(true);
-  };
-
-  const handleSearchFormBlur = () => {
-    setIsSearchFormFocused(false);
-  };
-  
   return (
-    <Wrapper>
-      <Left focused={isMemoFormFocused}>
-        <Link to="/">
-          <img src={homeIcon} alt="home" />
-        </Link>
+    <Header>
         <MemoForm
           focused={isMemoFormFocused}
-          onFocus={handleMemoFormFocus}
-          onBlur={handleMemoFormBlur}
+          onFocus={() => setIsMemoFormFocused(true)}
+          onBlur={() => setIsMemoFormFocused(false)}
         />
-      </Left>
-      <StyledSearchForm focused={isSearchFormFocused} onFocus={handleSearchFormFocus} onBlur={handleSearchFormBlur} />
-    </Wrapper>
+        <StyledSearchForm
+          focused={isSearchFormFocused}
+          onFocus={() => setIsSearchFormFocused(true)}
+          onBlur={() => setIsSearchFormFocused(false)}
+        />
+    </Header>
   )
 };
 

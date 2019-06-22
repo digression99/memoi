106bdbf9127a097f7df7b527c0882a43bd9c2683
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-22 23:55:38 +0900

    Add components in Group page

--- src/pages/Group/index.jsx
+++ src/pages/Group/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from 'components/Header';
 import LeftSideBar from 'components/LeftSideBar';
 import GroupItem from 'components/GroupItem';
 import { useSelector } from 'react-redux';
 import Page from 'elements/Page';
+import AddButton from 'components/AddButton';
+import GroupFormModal from 'components/GroupFormModal';
+import GroupForm from '../../components/GroupForm';
 
 const GroupContainer = styled.div`
   display: flex;
@@ -21,8 +24,20 @@ const GroupContainer = styled.div`
   padding : 4px;
 `;
 
+const StyledAddButton = styled(AddButton)`
+  position : fixed;
+  bottom : 40px;
+  right : 40px;
+`;
+
 export default () => {
   const groups = useSelector(state => state.groups);
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+  const handleClickAddButton = (e) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  };
 
   return (
     <Page.OuterWrapper>
@@ -32,7 +47,14 @@ export default () => {
         <GroupContainer>
           {groups.map(group => <GroupItem key={group.id} {...group} />)}
         </GroupContainer>
+        <StyledAddButton onClick={handleClickAddButton} />
       </Page.Wrapper>
+      {
+        isModalOpen &&
+        <GroupFormModal onClose={() => setIsModalOpen(false)}>
+          <GroupForm />
+        </GroupFormModal>
+      }
     </Page.OuterWrapper>
   )
 };

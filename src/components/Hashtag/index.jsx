dc4990a668323a54c6ea3958449857f8dafe8ddd
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-06-23 12:52:39 +0900

    Create Chip

--- src/components/Hashtag/index.jsx
+++ src/components/Hashtag/index.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Chip from 'elements/Chip';
 
 const Hashtag = styled(Link)`
-  display : inline-block;
-  background : #adb5bd;
-  border-radius : 100px;
-  padding : 4px 8px;
-  margin : 4px 4px 4px 0;
   color : #f1f3f5;
-  font-size : 0.8rem;
-
   &:link, &:visited {
     text-decoration : none;
   }
 `;
 
 export default ({ tag }) => {
-
   return (
-    <Hashtag to={`/hashtag/${tag}`}>{tag}</Hashtag>
+    <Chip>
+      <Hashtag to={`/hashtag/${tag}`}>{tag}</Hashtag>
+    </Chip>
   )
 };
---
+++ src/elements/Chip.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Chip = styled.div`
+  display : inline-block;
+  background : #adb5bd;
+  border-radius : 100px;
+  padding : 4px 8px;
+  margin : 4px 4px 4px 0;
+  font-size : 0.8rem;
+  color : #f1f3f5;
+`;
+
+export default Chip;
